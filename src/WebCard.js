d9e201d981d6e62a0b4d5f0fb8be3d2ba2abd46a
Tim Lu <timlu.coder@gmail.com>
2022-02-28 17:06:10 +0800

    [link projects / add assets]

--- src/WebCard.js
+++ src/WebCard.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const WebCard = ({ img, title, desp, projLink }) => {
   return (
-    <a href={projLink} target="_blank" className="card__link">
-      <img className="card__img" src={img} alt="project photo" />
+    <a href={projLink} target="_blank" className="card__link" rel="noreferrer">
+      <img className="card__img" src={img} alt={`project ${title}`} />
       <h2 className="card__title">{title}</h2>
       <p className="card__description">{desp}</p>
     </a>
