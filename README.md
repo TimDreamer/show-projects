65691c699c365d443e89ca3cfb02d1ef14c228e6
Tim Lu <tim.lu@linecrop.com>
2023-04-17 15:32:32 +0800

    add swiper-project

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# React Project
+# React / Vue Project
+
 #### Usage
+
 1. npm run build
 2. npm start
 
 #### Notes
-Show my portfolio with techs like html, css, javascript, react, redux and react-route.
+
+Show my portfolio with techs like html, css, javascript, react, redux, react-route and vue
 
 #### All Projects
+
 [Projects Site](https://show-projects.netlify.app/)
--- src/data.js
+++ src/data.js
@@ -1,40 +1,47 @@
-import autumnTravelImg from "./imgs/autumn_travel.png";
-import foodsImg from "./imgs/foods.png";
-import carSaleImg from "./imgs/car_sale.png";
-import nutritionImg from "./imgs/nutrition.png";
-import ytViewerImg from "./imgs/yt-viewer.png";
+import autumnTravelImg from './imgs/autumn_travel.png'
+import foodsImg from './imgs/foods.png'
+import carSaleImg from './imgs/car_sale.png'
+import nutritionImg from './imgs/nutrition.png'
+import ytViewerImg from './imgs/yt-viewer.png'
+import swiperDemoImg from './imgs/swiper_demo.png'
 
 const cards = [
   {
     img: autumnTravelImg,
-    title: "Autumn_Travel",
-    desp: "scss / float",
-    projLink: "https://autumn-travel.netlify.app",
+    title: 'Autumn_Travel',
+    desp: 'scss / float',
+    projLink: 'https://autumn-travel.netlify.app',
   },
   {
     img: foodsImg,
-    title: "Foods",
-    desp: "scss / flexbox",
-    projLink: "https://foods-view.netlify.app",
+    title: 'Foods',
+    desp: 'scss / flexbox',
+    projLink: 'https://foods-view.netlify.app',
   },
   {
     img: carSaleImg,
-    title: "Car_Sale",
-    desp: "scss / grid",
-    projLink: "https://car-sale-app.netlify.app",
+    title: 'Car_Sale',
+    desp: 'scss / grid',
+    projLink: 'https://car-sale-app.netlify.app',
   },
   {
     img: nutritionImg,
-    title: "Nutrition",
-    desp: "css / react / redux / JSON-server",
-    projLink: "https://nutritions-app.netlify.app/",
+    title: 'Nutrition',
+    desp: 'css / react / redux / JSON-server',
+    projLink: 'https://nutritions-app.netlify.app/',
   },
   {
     img: ytViewerImg,
-    title: "Yt-Viewer",
-    desp: "scss / react / redux",
-    projLink: "https://yt-viewer-app.netlify.app",
+    title: 'Yt_Viewer',
+    desp: 'scss / react / redux',
+    projLink: 'https://yt-viewer-app.netlify.app',
   },
-];
+  {
+    img: swiperDemoImg,
+    title: 'Swiper_Project',
+    desp: 'vue2 / swiper / colorThief',
+    projLink: 'https://swiper-project.netlify.app/',
+  },
+]
 
-export default cards;
+export default cards
